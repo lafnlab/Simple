6917ae9d72ed2074e2db34ea74c13ef636b912cf
Michael Hawkes <barista@gottahavacuppamocha.com>
2016-11-10 07:53:55 -0500

    Add  variable to connect.php

--- connect.php
+++ connect.php
@@ -4,4 +4,6 @@ define("D_HOST","");	// In most cases this will be "localhost".
 define("D_USER","");	// The name of the database user.
 define("D_PASS","");	// The password for the database user
 define("D_DATA","");	// The name of the database we will be using. It can be anything, but I recommend "simple".
+
+$lang = "en";		// use ISO-639-1 language code (https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes)
 ?>
