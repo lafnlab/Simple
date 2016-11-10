b8baaab2b8bab94814520feb5a6cbae08552e3dc
Michael Hawkes <barista@gottahavacuppamocha.com>
2016-11-10 07:27:20 -0500

    Add connect.php

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+// make these variables global
+define("D_HOST","");	// In most cases this will be "localhost".
+define("D_USER","");	// The name of the database user.
+define("D_PASS","");	// The password for the database user
+define("D_DATA","");	// The name of the database we will be using. It can be anything, but I recommend "simple".
+?>
